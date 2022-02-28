1dbe91796201414d630a40b30145aa96fa97e693
Александр Литвинов <sbawalexlit2@gmail.com>
2022-02-28 23:30:15 +0300

    нахождение максимального из трёх

---
+++ dz2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите третье число");
+int numberС = Convert.ToInt32(Console.ReadLine());
+
+int max=numberA;
+
+if (numberA>numberB) max=numberA;
+
+if (numberB>numberA) max=numberB;
+
+if (numberС>max) max=numberС;
+
+Console.WriteLine("максимальное число");
+Console.WriteLine(max);
+
