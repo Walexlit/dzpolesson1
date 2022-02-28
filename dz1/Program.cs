77c3ce515e835881efd3cad1e55bc6f662b93356
Александр Литвинов <sbawalexlit2@gmail.com>
2022-02-28 23:05:26 +0300

    нахождение большего и меньшего числа

---
+++ dz1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB)
+{
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+}
+else 
+{
+Console.WriteLine(numberB);
+Console.WriteLine(numberA);
+}
