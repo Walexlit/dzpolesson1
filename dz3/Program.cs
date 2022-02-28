8bf8196cd0bb6aa147952a25e643e3f83ed466d5
Александр Литвинов <sbawalexlit2@gmail.com>
2022-03-01 00:08:27 +0300

    опредиление чётных

---
+++ dz3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int rem = 0;
+rem = number % 2;
+if (rem == 0)
+{
+    Console.Write(number);
+    Console.Write(" число чётное");
+}
+else
+{
+    Console.Write(number);
+    Console.Write(" число не чётное");
+}
